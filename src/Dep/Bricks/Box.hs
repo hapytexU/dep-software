617337f4ea0ffacf905d5055ca3ea91c1ea8f068
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-03-28 14:36:43 +0200

    characters to create a box

---
+++ src/Dep/Bricks/Box.hs
@@ -0,0 +1,15 @@
+module Dep.Bricks.Box (
+    boxlb, boxlt, boxrb, boxrt
+  ) where
+
+boxlt :: Char
+boxlt = '\x250c'
+
+boxrt :: Char
+boxrt = '\x2510'
+
+boxlb :: Char
+boxlb = '\x2514'
+
+boxrb :: Char
+boxrb = '\x2518'
