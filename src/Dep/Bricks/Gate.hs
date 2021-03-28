e242adec2bf2f141f89751e8d3e415ee851d84ca
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-03-28 22:28:16 +0200

    horizontal gates

--- src/Dep/Bricks/Gate.hs
+++ src/Dep/Bricks/Gate.hs
@@ -1,6 +1,6 @@
 module Dep.Bricks.Gate where
 
-import Dep.Bricks.Box(boxhu, boxlt, boxrt, boxv)
+import Dep.Bricks.Box(boxh, boxhu, boxhd, boxlt, boxrt, boxlb, boxrb, boxv)
 import Dep.Bricks.Layout(CircuitLayout(Horizontal, Vertical))
 
 import Graphics.Vty.Attributes(Attr)
@@ -10,7 +10,8 @@ gateLine :: Char -> Char -> Char -> Int -> Attr -> Image
 gateLine c0 ci cn n = (`string` (c0 : replicate n ci ++ [cn]))
 
 gateH :: Char -> Int -> Attr -> Image
-gateH ci n attr = gateLine boxlt boxhu boxrt n attr <-> gateLine boxv ci boxv n attr
+gateH ci n attr = gateLine boxlt boxhu boxrt n attr <-> gateLine boxv ci boxv n attr <-> string attr (boxlb : replicate n2 boxh ++ boxhd : replicate (n-n2) boxh ++ [boxrb])
+    where n2 = div n 2
 
 gateV :: Char -> Int -> Attr -> Image
 gateV = undefined
