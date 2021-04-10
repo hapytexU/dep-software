e5af672b9a86255f7062a6ef3b6f8cb52c5e05ec
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-04-10 13:44:56 +0200

    implement a basic synthesis

--- src/Dep/Algorithm/Synthesis.hs
+++ src/Dep/Algorithm/Synthesis.hs
@@ -12,11 +12,23 @@ upper _ = True
 upperbound :: Three ThreeValue -> Three Bool
 upperbound = simplify . fmap upper
 
+_pushFalse :: Functor f => f [Maybe Bool] -> f [Maybe Bool]
+_pushFalse = fmap (Just False:)
+
+_pushTrue :: Functor f => f [Maybe Bool] -> f [Maybe Bool]
+_pushTrue = fmap (Just True:)
+
+{-guessProduct :: Three Bool -> Three ThreeValue -> Maybe ThreePath
+guessProduct (Leaf True) thr = [] <$ extractProduct thr
+guessPro
+guessProduct (Link l) s@(Split _ _) = extractProduct s
+guessProduct ()-}
+
 extractProduct :: Three ThreeValue -> Maybe ThreePath
 extractProduct (Leaf One) = Just []
 extractProduct (Leaf _) = Nothing
 extractProduct (Link l) = (Nothing :) <$> extractProduct l
-extractProduct ~(Split la lb) = ((Just False:) <$> extractProduct la) <|> ((Just True:) <$> extractProduct lb)
+extractProduct ~(Split la lb) = _pushFalse (extractProduct la) <|> _pushTrue (extractProduct lb)
 
 wipeout :: ThreePath -> Three ThreeValue -> Three ThreeValue
 wipeout path = simplify . applyTo (const DontCare) path
