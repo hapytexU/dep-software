974c93e4f661d911523e9512d8d26be696448e96
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-05-26 22:29:35 +0200

    work on the grey counter

---
+++ src/Dep/Data/GraySequence.hs
@@ -0,0 +1,13 @@
+module Dep.Data.GraySequence where
+
+import Data.Bits(Bits, (.&.), complement, complementBit, popCount, shiftL, xor)
+
+-- | Perform an increment by toggling only one bit (as a Gray counter is supposed to do).
+grayInc :: (Bits a, Ord a, Num a, Enum a) => Int -- ^ The given number of bits of the counter.
+    -> a -- ^ The given initial value of the counter.
+    -> a -- ^ The resulting value of the counter after increment.
+grayInc n b | even (popCount b) = complementBit b 0
+            | lst > lbm = xor b (shiftL lbm 1)
+            | otherwise = 0
+    where lbm = b .&. succ (complement b)
+          lst = shiftL 1 $ pred n
