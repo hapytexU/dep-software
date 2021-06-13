efe9965c5441040d6228bdf812348305ca6ea2cf
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-06-13 12:36:18 +0200

    added synthesis logic to the Karnaugh cards

--- src/Dep/Bricks/Karnaugh.hs
+++ src/Dep/Bricks/Karnaugh.hs
@@ -9,20 +9,32 @@ A module to render, update, and interact with Karnaugh cards.
 -}
 
 module Dep.Bricks.Karnaugh (
-    renderKarnaugh
+    renderKarnaugh, renderKarnaugh'
   ) where
 
+import Dep.Algorithm.Synthesis(synthesis)
 import Dep.Bricks.Utils(inRaster)
 import Dep.Class.Renderable(CharRenderable)
+import Dep.Data.Product(SumOfProducts)
 import Dep.Data.Three(Three)
+import Dep.Data.ThreeValue(ThreeValue)
 
 import Graphics.Vty.Attributes(Attr)
 import Graphics.Vty.Image(Image)
 
 -- | Render the given 'Three' as a /Karnaugh/ card, that can
 -- also visualize the /sum-of-product/.
-renderKarnaugh :: CharRenderable a
-  => Three a  -- ^ The given 'Three' to render as a /Karnaugh card/.
+renderKarnaugh'
+  :: Three ThreeValue  -- ^ The given 'Three' to render as a /Karnaugh card/.
   -> Attr  -- ^ The base 'Attr'ibute to render the /Karnaugh card/.
   -> Image  -- ^ The image that contains a rendered version of the /Karnaugh card/.
-renderKarnaugh _ _ = inRaster undefined undefined
+renderKarnaugh' = renderKarnaugh <*> synthesis
+
+-- | Render the given 'Three' as a /Karnaugh/ card, that can
+-- also visualize the /sum-of-product/.
+renderKarnaugh :: CharRenderable a
+  => Three a  -- ^ The given 'Three' to render as a /Karnaugh card/.
+  -> SumOfProducts -- ^ The sum of products that will be used to mark the /Karnaugh card/.
+  -> Attr  -- ^ The base 'Attr'ibute to render the /Karnaugh card/.
+  -> Image  -- ^ The image that contains a rendered version of the /Karnaugh card/.
+renderKarnaugh _ _ _ = inRaster undefined undefined
