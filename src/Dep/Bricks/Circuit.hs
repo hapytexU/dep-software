27f68d62462d29b81c307e8fc706ec73b3ddb142
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-06-16 21:17:02 +0200

    add markers for the Karnaugh card

--- src/Dep/Bricks/Circuit.hs
+++ src/Dep/Bricks/Circuit.hs
@@ -16,7 +16,7 @@ import Control.Lens.Operators((&), (.~)) -- , (^.))
 
 -- import Dep.Bricks.Gate(genericGate)
 -- import Dep.Bricks.Layout(CircuitLayout(Horizontal))
-import Dep.Bricks.Karnaugh(renderKarnaugh')
+import Dep.Bricks.Karnaugh(renderKarnaugh)
 import Dep.Data.Three(Three(Leaf, Split))
 import Dep.Data.ThreeValue(ThreeValue(Zero, One, DontCare))
 
@@ -32,4 +32,21 @@ circuit = Widget Fixed Fixed $ do
   let l0 = Leaf Zero
   let l1 = Leaf One
   let ld = Leaf DontCare
-  return (emptyResult & imageL .~ renderKarnaugh' (Split (Split (Split (Split l1 l0) l1) (Split (Split l0 l1) l1)) (Split (Split l1 ld) ld)) defAttr)
+  let la = Leaf 'A'
+  let lb = Leaf 'B'
+  let lc = Leaf 'C'
+  let ld = Leaf 'D'
+  let le = Leaf 'E'
+  let lf = Leaf 'F'
+  let lg = Leaf 'G'
+  let lh = Leaf 'H'
+  let li = Leaf 'I'
+  let lj = Leaf 'J'
+  let lk = Leaf 'K'
+  let ll = Leaf 'L'
+  let lm = Leaf 'M'
+  let ln = Leaf 'N'
+  let lo = Leaf 'O'
+  let lp = Leaf 'P'
+--   return (emptyResult & imageL .~ renderKarnaugh' (Split (Split (Split (Split l1 l0) l1) (Split (Split l0 l1) l1)) (Split (Split l1 ld) ld)) defAttr)
+  return (emptyResult & imageL .~ renderKarnaugh (Split (Split (Split (Split la lb) (Split lc ld)) (Split (Split le lf) (Split lg lh))) (Split (Split (Split li lj) (Split lk ll)) (Split (Split lm ln) (Split lo lp)))) mempty defAttr)
--- src/Dep/Bricks/Karnaugh.hs
+++ src/Dep/Bricks/Karnaugh.hs
@@ -15,7 +15,7 @@ module Dep.Bricks.Karnaugh (
   ) where
 
 import Dep.Algorithm.Synthesis(synthesis)
-import Dep.Bricks.Utils(inRaster)
+import Dep.Bricks.Utils(harrow', varrow', inRaster)
 import Dep.Class.Renderable(CharRenderable(charRenderItem))
 import Dep.Data.Product(SumOfProducts)
 import Dep.Data.Three(Three(Leaf, Link, Split), depth, leftmost)
@@ -23,32 +23,54 @@ import Dep.Data.ThreeValue(ThreeValue)
 import Dep.Utils(Operator)
 
 import Graphics.Vty.Attributes(Attr)
-import Graphics.Vty.Image(Image, (<->), emptyImage, string)
+import Graphics.Vty.Image(Image, (<->), (<|>), char, emptyImage, string)
 
 type KLine = String
 type KRaster = [KLine]
 
-mergeVertical :: KLine -> Operator KRaster
-mergeVertical _ ([], []) = []
-mergeVertical zs (xs@(x:_), []) = xs ++ [zs']
+stem :: Attr -> Image
+stem atr = string atr "\x2572 " <-> string atr " \x2572"
+
+-- spacemark :: Attr -> Int -> Image -> Image
+-- spacemark =
+
+hmark :: (Image -> Image -> Image) -> String -> Attr -> Int -> Image
+hmark f l atr n = f (string atr (replicate (div (n+3-length l) 2) ' ') <|> string atr l) (harrow' '\x2576' "\x2500" '\x2574' atr n)
+
+hmark' :: String -> Attr -> Int -> Image
+hmark' = hmark (<->)
+
+hmark'' :: String -> Attr -> Int -> Image
+hmark'' = hmark (flip (<->))
+
+vmark :: (Image -> Image -> Image) -> String -> Attr -> Int -> Int -> Image
+vmark _ _ atr m n = foldr ((<->) . char atr) (varrow' '\x2577' "\x2502" '\x2575' atr n) (replicate m ' ')
+
+vmark' :: String -> Attr -> Int -> Int -> Image
+vmark' = vmark (<|>)
+
+vmark'' :: String -> Attr -> Int -> Int -> Image
+vmark'' = vmark (flip (<|>))
+
+_mergeVertical :: KLine -> Operator KRaster
+_mergeVertical _ ([], []) = []
+_mergeVertical zs (xs@(x:_), []) = xs ++ [zs']
   where zs' = zipWith const (cycle zs) x
-mergeVertical zs (xs, ys@(y:_)) = xs ++ zs' : reverse ys
+_mergeVertical zs (xs, ys@(y:_)) = xs ++ zs' : reverse ys
   where zs' = zipWith const (cycle zs) y
 
-mergeHorizontal :: KLine -> Operator KRaster
-mergeHorizontal spl = uncurry (zipWith3 f (cycle spl))
+_mergeHorizontal :: KLine -> Operator KRaster
+_mergeHorizontal spl = uncurry (zipWith3 f (cycle spl))
   where f sp xs ys = xs ++ sp : reverse ys
 
-recurse :: CharRenderable a => Operator KRaster -> Operator KRaster -> Int -> Three a -> KRaster
-recurse ma mb !n = go
-      where fn = recurse mb ma (n-1)
+_recurse :: CharRenderable a => Operator KRaster -> Operator KRaster -> Int -> Three a -> KRaster
+_recurse ma mb !n = go
+      where fn = _recurse mb ma (n-1)
             go l |
               n <= 0 = [[charRenderItem (leftmost l)]]  -- leftmost is used to prevent cases with multiple items
             go l@(Leaf _) = let fnl = fn l in ma (fnl, fnl)
             go (Link l) = let fnl = fn l in ma (fnl, fnl)
             go (Split la lb) = ma (fn la, fn lb)
-
--- renderCard :: CharRenderable a => Three a -> String
 
 -- | Render the given 'Three' as a /Karnaugh/ card, that can
 -- also visualize the /sum-of-product/.
@@ -65,5 +87,5 @@ renderKarnaugh :: CharRenderable a
   -> SumOfProducts -- ^ The sum of products that will be used to mark the /Karnaugh card/.
   -> Attr  -- ^ The base 'Attr'ibute to render the /Karnaugh card/.
   -> Image  -- ^ The image that contains a rendered version of the /Karnaugh card/.
-renderKarnaugh ts _ atr = inRaster atr (foldr ((<->) . string atr) emptyImage recs)
-  where recs = recurse (mergeHorizontal "\x2502\x253c") (mergeVertical "\x2500\x253c") (depth ts) ts
+renderKarnaugh ts _ atr = stem atr <|> (((string atr "     " <|> hmark' "x\x2081" atr 3) <-> (vmark' "x\x2082" atr 4 3 <|> inRaster atr (foldr ((<->) . string atr) emptyImage recs) <|> emptyImage ))) <-> (string atr "   " <|> hmark'' "x\x2083" atr 3)
+  where recs = _recurse (_mergeHorizontal "\x2502\x253c") (_mergeVertical "\x2500\x253c") (depth ts) ts
